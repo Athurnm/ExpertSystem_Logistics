ca2339c85e78bf6a0c03c5f2bf1fd067d1b98398
Athur Muharam <athurnm@gmail.com>
2019-10-16 23:10:42 +0700

    fix error lazy loading

--- Websystemview/app.py
+++ Websystemview/app.py
@@ -1,15 +1,19 @@
 from flask import Flask,render_template, request,jsonify,Response
-from clips import Environment
+# from clips import Environment
 
 # Initiate the app of web server
 app = Flask(__name__)
+
+# initiate and load expert system
+# env = Environment()
+# env.load("Rule.clp")
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/Questions")
-def Questions():
+@app.route("/questions")
+def questions():
     return "Is there any questions?"
 
 if __name__ == "__main__":
